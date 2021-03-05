5fc6d03e2ce8dffbfa56900391ece90800d868c5
Mads Moi-Aune <609372+madsaune@users.noreply.github.com>
2021-03-05 13:36:03 +0100

    refactor new message

--- pswincom_test.go
+++ pswincom_test.go
@@ -9,11 +9,7 @@ import (
 func TestSendMessage(t *testing.T) {
 	client := NewClient(os.Getenv("PSWINCOM_USER"), os.Getenv("PSWINCOM_PASSWORD"), os.Getenv("PSWINCOM_SENDER"), nil)
 
-	message := &Message{
-		Recipient: os.Getenv("PSWINCOM_RECIPIENT_TEST"),
-		Body:      "This is a test! With norwegian letters: æøå",
-	}
-
+	message := NewMessage(os.Getenv("PSWINCOM_RECIPIENT_TEST"), "This is a test! With norwegian letters: æøå", false)
 	err := client.SendMessage(message)
 
 	if err != nil {
